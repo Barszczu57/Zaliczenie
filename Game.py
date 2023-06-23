b2d33749fc06e577b286ffa5834d10993c6aea36
Bartek <74347328+Barszczu57@users.noreply.github.com>
2023-06-23 17:04:27 +0200

    Końcowa aktualizacja
    Pełna funkcjonalność programów oraz końcowe poprawki

--- Game.py
+++ Game.py
@@ -1,2 +1,10 @@
+import random
 print("Zagrajmy w gre!")
 zgaduj = input("Zgadnij jaką liczbę z przedziału od 1 do 10 wybiorę: ")
+zgaduj = int(zgaduj)
+liczba = random.randint(1,10)
+if zgaduj == liczba:
+    print(f"Brawo zgadłeś/aś! Ta liczba to: {liczba}")
+else:
+    print(f"Nie zgadłeś/aś :(. Ta liczba to: {liczba}")
+    
--- Welcome.py
+++ Welcome.py
@@ -1,3 +1,4 @@
 print("Witaj!")
-imie = input("Jak się nazywasz? B")
+imie = input("Jak się nazywasz? ")
 print(f"Miło mi cię poznać {imie}.")
+print(f"Zachęcam Cię {imie} do sprawdzenia mojego drugiego programu o nazwie 'Game'")
